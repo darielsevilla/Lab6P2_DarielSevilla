07341a139ca99a6520e1baa7252a31626236b191
Dariel Sevilla <darielsevilla@gmail.com>
2023-03-03 13:40:35 -0600

    Diseño de login screen

---
+++ src/lab6p2_darielsevilla/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2_darielsevilla;
+
+/**
+ *
+ * @author HP
+ */
+public class Usuario {
+    private String username;
+    private float password;
+    private int edad;
+
+    public Usuario() {
+    }
+    
+    
+}
